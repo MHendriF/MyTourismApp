859beb750250dc28a78da97fc4d5770ee74575df
MHendriF <hendrifebriansyah28@gmail.com>
2021-01-13 22:27:20 +0700

    implement SQLCipher done

--- core/src/main/java/com/mhendrif/mytourismapp/core/di/CoreModule.kt
+++ core/src/main/java/com/mhendrif/mytourismapp/core/di/CoreModule.kt
@@ -8,6 +8,8 @@ import com.mhendrif.mytourismapp.core.data.source.remote.RemoteDataSource
 import com.mhendrif.mytourismapp.core.data.source.remote.network.ApiService
 import com.mhendrif.mytourismapp.core.domain.repository.ITourismRepository
 import com.mhendrif.mytourismapp.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,27 +21,31 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<TourismDatabase>().tourismDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("mhendrif".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
-            androidContext(),
-            TourismDatabase::class.java, "Tourism.db"
-        ).fallbackToDestructiveMigration().build()
+                androidContext(),
+                TourismDatabase::class.java, "Tourism.db"
+        ).fallbackToDestructiveMigration()
+                .openHelperFactory(factory)
+                .build()
     }
 }
 
 val networkModule = module {
     single {
         OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-            .connectTimeout(120, TimeUnit.SECONDS)
-            .readTimeout(120, TimeUnit.SECONDS)
-            .build()
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .build()
     }
     single {
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://tourism-api.dicoding.dev/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(get())
-            .build()
+                .baseUrl("https://tourism-api.dicoding.dev/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(get())
+                .build()
         retrofit.create(ApiService::class.java)
     }
 }
