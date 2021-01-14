c659a2e034517355dca5eb20e05734f4782f6149
MHendriF <hendrifebriansyah28@gmail.com>
2021-01-14 16:52:48 +0700

    implement Certificate Pinning done

--- core/src/main/java/com/mhendrif/mytourismapp/core/di/CoreModule.kt
+++ core/src/main/java/com/mhendrif/mytourismapp/core/di/CoreModule.kt
@@ -10,6 +10,7 @@ import com.mhendrif.mytourismapp.core.domain.repository.ITourismRepository
 import com.mhendrif.mytourismapp.core.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -24,28 +25,35 @@ val databaseModule = module {
         val passphrase: ByteArray = SQLiteDatabase.getBytes("mhendrif".toCharArray())
         val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
-                androidContext(),
-                TourismDatabase::class.java, "Tourism.db"
+            androidContext(),
+            TourismDatabase::class.java, "Tourism.db"
         ).fallbackToDestructiveMigration()
-                .openHelperFactory(factory)
-                .build()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "tourism-api.dicoding.dev"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/paJOw+DTCx1KaSMeALtM5gXuxJN4lP04qMKhSXBFa9Y=")
+            .add(hostname, "sha256/qPerI4uMwY1VrtRE5aBY8jIQJopLUuBt2+GDUWMwZn4=")
+            .add(hostname, "sha256/iie1VXtL7HzAMF+/PVPR9xzT80kQxdZeJ+zduCB3uj0=")
+            .build()
         OkHttpClient.Builder()
-                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-                .connectTimeout(120, TimeUnit.SECONDS)
-                .readTimeout(120, TimeUnit.SECONDS)
-                .build()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
     }
     single {
         val retrofit = Retrofit.Builder()
-                .baseUrl("https://tourism-api.dicoding.dev/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(get())
-                .build()
+            .baseUrl("https://tourism-api.dicoding.dev/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
         retrofit.create(ApiService::class.java)
     }
 }
