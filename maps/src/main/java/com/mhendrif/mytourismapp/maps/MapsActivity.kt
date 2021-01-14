c40293ddfbd3b7de9585b77122b435b1c9383851
MHendriF <hendrifebriansyah28@gmail.com>
2021-01-14 21:14:33 +0700

    init project

--- maps/src/main/java/com/mhendrif/mytourismapp/maps/MapsActivity.kt
+++ maps/src/main/java/com/mhendrif/mytourismapp/maps/MapsActivity.kt
@@ -1,28 +1,57 @@
 package com.mhendrif.mytourismapp.maps
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.mhendrif.mytourismapp.core.data.Resource
+import com.mhendrif.mytourismapp.core.domain.model.Tourism
+import com.mhendrif.mytourismapp.detail.DetailTourismActivity
 import com.mhendrif.mytourismapp.maps.databinding.ActivityMapsBinding
 import com.mhendrif.mytourismapp.maps.di.mapsModule
+import com.google.gson.Gson
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.geometry.LatLngBounds
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.context.loadKoinModules
 
+
+
 class MapsActivity : AppCompatActivity() {
 
+    companion object {
+        private const val ICON_ID = "ICON_ID"
+    }
+
+    private lateinit var mapboxMap: MapboxMap
     private val mapsViewModel: MapsViewModel by viewModel()
     private lateinit var binding: ActivityMapsBinding
+    private val mapboxToken: String = "pk.eyJ1IjoibWhlbmRyaWYiLCJhIjoiY2tqMmgwNTA3MGJqYTMwcnN0YjNuZXAzaSJ9.Okr2nOyk9-ggnilBfvZZvQ"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Mapbox.getInstance(
+            this,
+            mapboxToken
+        )
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         loadKoinModules(mapsModule)
 
         supportActionBar?.title = "Tourism Map"
-        getTourismData()
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync { mapboxMap ->
+            this.mapboxMap = mapboxMap
+            getTourismData()
+        }
     }
 
     private fun getTourismData() {
@@ -32,7 +61,7 @@ class MapsActivity : AppCompatActivity() {
                     is Resource.Loading -> binding.progressBar.visibility = View.VISIBLE
                     is Resource.Success -> {
                         binding.progressBar.visibility = View.GONE
-                        binding.tvMaps.text = "This is map of ${tourism.data?.get(0)?.name}"
+                        showMarker(tourism.data)
                     }
                     is Resource.Error -> {
                         binding.progressBar.visibility = View.GONE
@@ -42,5 +71,63 @@ class MapsActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    private fun showMarker(dataTourism: List<Tourism>?) {
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+            style.addImage(ICON_ID, BitmapFactory.decodeResource(resources, R.drawable.mapbox_marker_icon_default))
+            val latLngBoundsBuilder = LatLngBounds.Builder()
+            val symbolManager = SymbolManager(binding.mapView, mapboxMap, style)
+            symbolManager.iconAllowOverlap = true
+            val options = ArrayList<SymbolOptions>()
+            dataTourism?.forEach { data ->
+                latLngBoundsBuilder.include(LatLng(data.latitude, data.longitude))
+                options.add(
+                    SymbolOptions()
+                        .withLatLng(LatLng(data.latitude, data.longitude))
+                        .withIconImage(ICON_ID)
+                        .withData(Gson().toJsonTree(data))
+                )
+            }
+            symbolManager.create(options)
+            val latLngBounds = latLngBoundsBuilder.build()
+            mapboxMap.easeCamera(CameraUpdateFactory.newLatLngBounds(latLngBounds, 50), 5000)
+            symbolManager.addClickListener { symbol ->
+                val data = Gson().fromJson(symbol.data, Tourism::class.java)
+                val intent = Intent(this, DetailTourismActivity::class.java)
+                intent.putExtra(DetailTourismActivity.EXTRA_DATA, data)
+                startActivity(intent)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 }
